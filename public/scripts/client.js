c9755b38bfdd1d84fdcd3dbc5790151b40c36d1c
Hamza Warsame <hamza360123@gmail.com>
2021-11-17 18:53:25 -0500

    remove container from html and put it in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,53 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() { 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+const createTweetElement = function(data){
+ return `<article class="tweet">
+        <div class="tweetHeader">
+          <span class="avatar"><img src=${data.user.avatars}><span class="tweetername">${data.user.name}</span></span>
+          <span class="username">${data.user.handle}</span>
+        </div>
+        <p class="tweetInput"> ${data.content.text}</p>
+        <hr class="divider">
+        <div class="tweetFooter">
+          <span class="timestamp">${timeago.format(data.created_at)} </span>
+          <div class="icons">
+            <i class="fas fa-heart"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-bookmark"></i>
+          </div>
+        </div>
+      </article>`
+}
 
+
+const renderTweets = function(tweets) {
+  for (let post of tweets){
+   let newTweet = createTweetElement(post)
+    $('.postTweet').append(newTweet);
+  }
+}
+renderTweets(data);
+});
